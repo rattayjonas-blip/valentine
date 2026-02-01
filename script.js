cdd6fb49082ac0a1c60092a3e311430410122ea3
rattayjonas-blip <rattay.jonas@gmail.com>
2026-02-01 13:09:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+const image = document.getElementById("mainImage");
+const title = document.getElementById("title");
+
+let yesSize = 1;
+
+noBtn.addEventListener("click", () => {
+  yesSize += 0.2;
+  yesBtn.style.transform = `scale(${yesSize})`;
+});
+
+yesBtn.addEventListener("click", () => {
+  title.textContent = "YAAAYYY 💕";
+  image.src = "yay.jpg";
+  noBtn.style.display = "none";
+  yesBtn.style.display = "none";
+});
